6fc1e660617b2241d2f0140d5134803b42886079
Piotr Szerszeń <szersze.piotr3@gmail.com>
2017-10-25 00:04:20 +0200

    http client; arrays utils

--- src/main/java/com/osa/java9test/Java9.java
+++ src/main/java/com/osa/java9test/Java9.java
@@ -5,18 +5,30 @@ import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
 
 import java.io.IOException;
+import java.net.ProxySelector;
 import java.net.URI;
+import java.time.Duration;
 
 public class Java9 {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpResponse<String> response = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.SAME_PROTOCOL)
+                .proxy(ProxySelector.getDefault())
+                .version(HttpClient.Version.HTTP_1_1)
+//                .authenticator(new Authenticator() {
+//                    @Override
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        return new PasswordAuthentication("user", "passwd".toCharArray());
+//                    }
+//                })
                 .build()
                 .send(
-                        HttpRequest.newBuilder(URI.create("https://www.google.pl"))
+                        HttpRequest.newBuilder(URI.create("https://www.pgs-soft.com/pl/"))
                                 .header("User-Agent", "Java 9")
                                 .header("Accept", "text/html; charset=UTF-8")
+                                .timeout(Duration.ofSeconds(5))
+//                                .POST(HttpRequest.BodyProcessor.from)
                                 .GET()
                                 .build(),
                         HttpResponse.BodyHandler.asString());
---
+++ src/test/java/com/osa/java9test/ArraysImprovementsTest.java
@@ -0,0 +1,79 @@
+package com.osa.java9test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.compare;
+import static java.util.Arrays.mismatch;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ArraysImprovementsTest {
+    @Test
+    void arrayEqualsTest() {
+        int[] existRows = { 0, 1, 2, 3, 4, 5 };
+        int[] newRows = { 3, 4, 5, 1, 2, 0 };
+
+        assertAll(() -> assertFalse(Arrays.equals(existRows, newRows)),
+                () -> assertTrue(Arrays.equals(
+                        existRows, 1, 3,
+                        newRows, 3, 5)),
+                () -> assertTrue(Arrays.equals(
+                        existRows, 3, 5,
+                        newRows, 0, 2)));
+    }
+
+    @Test
+    void compareArraysTest() {
+        int[] tomMarks = { 5, 6, 7, 8, 9, 10 };
+        int[] aliceMarks = { 5, 6, 7, 8, 9, 10 };
+        int[] daisyMarks = { 5, 6, 7, 10, 9, 10 };
+        int[] maryMarks = { 5, 6, 7, 8 };
+
+        assertAll(() -> assertEquals(0, compare(tomMarks, aliceMarks)),
+                () -> assertEquals(-1, compare(tomMarks, daisyMarks)),
+                () -> assertEquals(2, compare(tomMarks, maryMarks)));
+    }
+
+    @Test
+    void compareSliceArraysTest() {
+        int[] tomMarks = { 5, 6, 7, 8, 9, 10 };
+        int[] daisyMarks = { 5, 6, 7, 10, 9, 10 };
+        int[] maryMarks = { 5, 6, 7, 8 };
+
+        assertAll(
+                () -> assertEquals(0, compare(
+                        tomMarks, 0, 3,
+                        daisyMarks, 0, 3)),
+                () -> assertEquals(0, compare(
+                        tomMarks, 0, 4,
+                        maryMarks, 0, maryMarks.length)),
+                () -> assertEquals(1, compare(
+                        daisyMarks, 0, 4,
+                        maryMarks, 0, maryMarks.length)));
+
+    }
+
+    @Test
+    void mismatchArraysTest() {
+        int[] a = { 1, 2, 3, 4, 5 };
+        int[] b = { 1, 2, 3, 4, 5 };
+        int[] c = { 1, 2, 4, 4, 5, 6 };
+
+        assertAll(
+                () -> assertEquals(-1, mismatch(a, b)),
+                () -> assertEquals(2, mismatch(a, c)),
+                () -> assertEquals(-1, mismatch(
+                        a, 0, 2,
+                        c, 0, 2)),
+                () -> assertEquals(2, mismatch(
+                        a, 0, 3,
+                        c, 0, 3)),
+                () -> assertEquals(0, mismatch(
+                        a, 2, a.length,
+                        c, 2, 5)));
+    }
+}
