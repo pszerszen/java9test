76423aecd9c5fb09b35ec9a6e5580955e9cf89e5
Piotr Szerszeń <szersze.piotr3@gmail.com>
2018-01-31 22:16:18 +0100

    httpc client features

--- src/main/java/com/osa/java9test/Java9.java
+++ src/main/java/com/osa/java9test/Java9.java
@@ -5,33 +5,39 @@ import jdk.incubator.http.HttpRequest;
 import jdk.incubator.http.HttpResponse;
 
 import java.io.IOException;
+import java.net.CookieManager;
 import java.net.ProxySelector;
 import java.net.URI;
 import java.time.Duration;
+import java.util.concurrent.Executors;
 
 public class Java9 {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpResponse<String> response = HttpClient.newBuilder()
+        HttpClient httpClient = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.SAME_PROTOCOL)
                 .proxy(ProxySelector.getDefault())
-                .version(HttpClient.Version.HTTP_1_1)
+                .version(HttpClient.Version.HTTP_2)
+                .executor(Executors.newCachedThreadPool())
+                .priority(256)
+                .cookieManager(new CookieManager())
 //                .authenticator(new Authenticator() {
 //                    @Override
 //                    protected PasswordAuthentication getPasswordAuthentication() {
 //                        return new PasswordAuthentication("user", "passwd".toCharArray());
 //                    }
 //                })
-                .build()
-                .send(
-                        HttpRequest.newBuilder(URI.create("https://www.pgs-soft.com/pl/"))
-                                .header("User-Agent", "Java 9")
-                                .header("Accept", "text/html; charset=UTF-8")
-                                .timeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create("https://www.pgs-soft.com/pl/"))
+                .header("User-Agent", "Java 9")
+                .header("Accept", "text/html; charset=UTF-8")
+                .timeout(Duration.ofSeconds(5))
 //                                .POST(HttpRequest.BodyProcessor.from)
-                                .GET()
-                                .build(),
-                        HttpResponse.BodyHandler.asString());
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandler.asString());
         System.out.println(response.body());
     }
 }
